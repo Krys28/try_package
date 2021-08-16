df520240a7095d248482a954274dc7c3ca44bf43
Krystelle Gilard <k.gilard@yahoo.fr>
2021-08-16 15:27:59 +0200

    Initial Push

---
+++ try_package/lib.py
@@ -0,0 +1,8 @@
+def circle_area(r):
+    PI = 3.141592653
+    area_c = PI * (r * r)
+    return print(f"Area of circle (r={r}) = {area_c}")
+
+if __name__ == "__main__":
+    r=6
+    area_c = circle_area(r)
